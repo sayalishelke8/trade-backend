9f3635b4ae38b78bc4fd276729d0865bfdf8b195
sayalish <73734054+sayalishelke8@users.noreply.github.com>
2025-11-18 16:14:28 +0530

    Refactor server listen to use PORT variable
    Updated server to use a PORT variable for flexibility.

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => res.send('Backend running'));
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
