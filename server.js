0af8ff78d756f2ce1cf2fce205e5da8c7cb92107
sayalish <73734054+sayalishelke8@users.noreply.github.com>
2025-11-18 13:09:44 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => res.send('Backend running'));
+app.listen(5000, () => console.log('Server running on port 5000'));
